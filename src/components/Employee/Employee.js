ee3653f8b5f14c77dc807f232d9e703ad139d989
YuRo <92213643+YuRo711@users.noreply.github.com>
2024-05-28 23:28:13 +0300

    employee styles

--- src/components/Employee/Employee.js
+++ src/components/Employee/Employee.js
@@ -5,6 +5,19 @@ function Employee(props) {
 
   return (
     <div className="employee">
+      <div className="employee__header">
+        <div className="employee__portrait">
+          <img className="employee__image"
+            src={image}
+            alt="employee portrait"
+          />
+        </div>
+        <div className="employee__info">
+          <h4 className="employee__name">{name}</h4>
+          <p className="employee__job">{job}</p>
+        </div>
+      </div>
+      <p className="employee__text">{text}</p>
     </div>
   );
 }
