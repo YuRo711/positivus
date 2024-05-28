36ab56b585d0baef01e971613bb4b203ac039b36
YuRo <92213643+YuRo711@users.noreply.github.com>
2024-05-28 23:45:46 +0300

    employees button

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import { employees } from "../../utils/constants";
 import Employee from "../Employee/Employee";
 import "./Team.css";
 
-
 function Team(props) {
+  function openAll() {
+    setCardsShown(employees.length);
+  }
+
+  const [cardsShown, setCardsShown] = useState(3);
+
   return (
     <div className="team">
       <div className="team__cards">
@@ -17,8 +23,16 @@ function Team(props) {
               text={data.text}
             />
           })
+          .slice(0, cardsShown)
         }
       </div>
+      <button className={`black-button team__button 
+        ${cardsShown > 3 ? "team__button_hidden" : ""}`}
+        type="button"
+        onClick={openAll}
+      >
+        See all team
+      </button>
     </div>
   );
 }
